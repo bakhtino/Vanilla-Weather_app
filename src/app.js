393cec62551f78e58d8f6fb9b46202be0174c217
bakhtino <bakhtino11@gmail.com>
2023-02-09 17:57:04 +0200

    add relevant weather icon

--- src/app.js
+++ src/app.js
@@ -18,11 +18,14 @@
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let iconElement = document.querySelector("#icon");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);  
+    iconElement.setAttribute("src", ` http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);  
 }
 let city = "New York";
 let apiKey = "be3787b39239779c9856215f2383d86b";
